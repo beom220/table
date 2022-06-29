b5b810f68c8f65e4efb09a21b10bee90a573397d
beom220 <beom220@gmail.com>
2022-06-29 23:24:02 +0900

    [ feat ] util time func

---
+++ client/src/components/utils/today.js
@@ -0,0 +1,32 @@
+function useToday(){
+    const year = new Date().getFullYear();
+    const month = ("0" + (1 + new Date().getMonth())).slice(-2);
+    const date = ("0" + new Date().getDate()).slice(-2);
+    return year + month + date;
+}
+
+
+function useTimeForToday(value) {
+    const today = new Date();
+    const timeValue = new Date(value);
+
+    const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+    if (betweenTime < 1) return '방금전';
+    if (betweenTime < 60) {
+        return `${betweenTime}분전`;
+    }
+
+    const betweenTimeHour = Math.floor(betweenTime / 60);
+    if (betweenTimeHour < 24) {
+        return `${betweenTimeHour}시간전`;
+    }
+
+    const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+    if (betweenTimeDay < 365) {
+        return `${betweenTimeDay}일전`;
+    }
+
+    return `${Math.floor(betweenTimeDay / 365)}년전`;
+}
+
+export { useTimeForToday, useToday}
